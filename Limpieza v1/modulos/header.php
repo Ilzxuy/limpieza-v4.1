aa20147bcf3f186820b714c72959a4b5c78d22d2
Janpihero <150623262+Ilzxuy@users.noreply.github.com>
2024-07-05 19:39:53 -0600

    Add files via upload

---
+++ Limpieza v1/modulos/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Venta de servicios de seguidores al mejor precio y de cualquier red social">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
+    <link rel="icon" href="assets/img/logo-removebg-preview.png">
+    <link rel="stylesheet" href="assets/styles.css">
+    <title>limpieza</title>
+</head>
+<body>
+
+    <!-- Barra de navegación -->
+    <header id="navbar">
+    <div class="contenido-header">
+        <h1>Radiance Cleaning Services LLC.</h1>
+        <nav id="nav" class="">
+            <ul id="links">
+                <li><a href="index.php" class="seleccionado" onclick="seleccionar(this)">HOME</a></li>
+                <li><a href="servicios.php" onclick="seleccionar(this)">SERVICES</a></li>
+                <li><a href="fotos.php" onclick="seleccionar(this)">PHOTOS</a></li>                
+                <li><a href="index.php#sobre-contacto" onclick="seleccionar(this)">ABOUT US</a></li>
+            </ul>
+        </nav>
+        <div id="icono-nav" onclick="responsiveMenu()">
+            <img src="assets/img/menu-bar.png" alt="Icono Personalizado">
+        </div>
+        <div class="redes">
+            <a href="#"><img src="assets/img/facebook1.png" alt="Facebook"></a>
+            <a href="#"><img src="assets/img/snapchat2.png" alt="YouTube"></a>
+            <a href="#"><img src="assets/img/instagram1.png" alt="Instagram"></a>
+        </div>
+    </div>
+</header>
+
+
